ad18570e5ed42e1207c1c3b13d9ce6a571f7509d
Rijk Ravestein <rijkr@savapage.org>
2017-05-10 19:10:56 +0200

    Add OAuth plugin interface (in progress)
    See: #822

--- src/main/java/org/savapage/core/dao/enums/ExternalSupplierEnum.java
+++ src/main/java/org/savapage/core/dao/enums/ExternalSupplierEnum.java
@@ -1,6 +1,6 @@
 /*
- * This file is part of the SavaPage project <http://savapage.org>.
- * Copyright (c) 2011-2014 Datraverse B.V.
+ * This file is part of the SavaPage project <https://www.savapage.org>.
+ * Copyright (c) 2011-2017 Datraverse B.V.
  * Author: Rijk Ravestein.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,7 +14,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * For more information, please contact Datraverse B.V. at this
  * address: info@datraverse.com
@@ -29,12 +29,17 @@ package org.savapage.core.dao.enums;
 public enum ExternalSupplierEnum {
 
     /**
+     * G.
+     */
+    GOOGLE("Google", "google.png"),
+
+    /**
      * SavaPage in role as external supplier.
      */
     SAVAPAGE("SavaPage", "savapage.png"),
 
     /**
-     * Smartschool Afdrukcentrum.
+     * Smartschool.
      */
     SMARTSCHOOL("Smartschool", "smartschool.jpg");
 
