770c909a96887a0b6246c2bc895c34ed22f576c9
Rijk Ravestein <rijkr@savapage.org>
2020-09-17 11:05:59 +0200

    Edit delegated print copies of job ticket
    See: 1132

--- src/main/java/org/savapage/core/config/IConfigProp.java
+++ src/main/java/org/savapage/core/config/IConfigProp.java
@@ -3187,11 +3187,6 @@ public interface IConfigProp {
                 "webapp.jobtickets.reopen.enable", BOOLEAN_VALIDATOR, V_NO,
                 API_UPDATABLE_ON),
 
-        /** */
-        WEBAPP_JOBTICKETS_COPIES_EDIT_ENABLE(//
-                "webapp.jobtickets.copies-edit.enable.1", BOOLEAN_VALIDATOR,
-                V_NO, API_UPDATABLE_ON),
-
         /**
          * Number of job tickets to show in the list. A value of zero means all
          * available tickets are shown.
